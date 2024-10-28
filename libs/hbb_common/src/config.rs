88d28d75251124c06902c24bbfec3875fedac27d
xzhslim <61769932+xzhslim@users.noreply.github.com>
2024-10-28 22:46:35 +0800

    Update config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -72,7 +72,7 @@ lazy_static::lazy_static! {
     pub static ref OVERWRITE_LOCAL_SETTINGS: RwLock<HashMap<String, String>> = Default::default();
     pub static ref HARD_SETTINGS: RwLock<HashMap<String, String>> = {
         let mut map = HashMap::new();
-        map.insert("password".to_string(), "Rus@slimg0".to_string());
+        map.insert("password".to_string(), "ZXKJ@rustdesk20140109".to_string());
         RwLock::new(map)
     };
 
